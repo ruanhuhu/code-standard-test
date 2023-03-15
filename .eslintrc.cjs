cc3d52078a27489bc07d847e4acdb6cea5301b9e
ruanhuhu <ruanhuhu127@gmail.com>
2023-03-15 14:27:30 +0800

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,39 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  'overrides': [],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-  },
-  'plugins': [
-    'vue',
-    '@typescript-eslint'
+  plugins: ['vue', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-console': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            vars: 'all',
+            args: 'after-used',
+            ignoreRestSiblings: true,
+          },
+        ],
+      },
+    },
   ],
-  'rules': {}
+  rules: {},
 }
