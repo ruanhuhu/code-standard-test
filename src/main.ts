34384e2d6cda3664dc2499567b30a9a6ce9d4dcd
ruanhuhu <ruanhuhu127@gmail.com>
2023-03-16 17:38:30 +0800

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-const a = 'sss'
-console.log('1111')
+// const a = 'sss'
+// console.log('1111')
 
 createApp(App).mount('#app')
