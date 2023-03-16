b708d696bf7d6aa4dec33cca4b051973673a1c7e
ruanhuhu <ruanhuhu127@gmail.com>
2023-03-16 17:37:07 +0800

    eslint规则配置更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,12 +9,13 @@ module.exports = {
     sourceType: 'module',
   },
   extends: [
-    'plugin:vue/vue3-recommended',
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint', 'import'],
   overrides: [
     {
       files: ['*.vue'],
@@ -22,18 +23,46 @@ module.exports = {
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
-      rules: {
-        'no-console': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            vars: 'all',
-            args: 'after-used',
-            ignoreRestSiblings: true,
-          },
-        ],
-      },
     },
   ],
-  rules: {},
+  rules: {
+    // js/ts
+    camelcase: ['error', { properties: 'never' }],
+    'no-console': ['warn', { allow: ['error'] }],
+    'no-debugger': 'warn',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'no-return-await': 'error',
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'prefer-const': [
+      'warn',
+      { destructuring: 'all', ignoreReadBeforeAssign: true },
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      { allowNamedFunctions: false, allowUnboundThis: true },
+    ],
+    'object-shorthand': [
+      'error',
+      'always',
+      { ignoreConstructors: false, avoidQuotes: true },
+    ],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'no-unused-vars': [2, { ignoreRestSiblings: true }],
+    'no-undef': 2,
+
+    // ts
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    // import
+    'import/no-unresolved': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/named': 'off',
+  },
 }
